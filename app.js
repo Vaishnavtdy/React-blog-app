b17f8745a9d1b72079e51232dac7cec011bffdaf
unknown <vaishnavtdy@gmail.com>
2022-07-26 11:17:17 +0530

    Addind edited files

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ import router from "./routes/user-routes";
 import cors from 'cors'
 
 const app = express();
+const PORT = process.env.PORT || 8000
 app.use(cors())
 app.use(express.json())
 app.use("/api/user", router)
 app.use("/api/blog", blogRouter)
 mongoose
-  .connect(
+  .connect( process.env.MONGODB_URI ||
     "mongodb+srv://Admin:CRuOAqn5vCzXm3pY@cluster0.jiedk1k.mongodb.net/?retryWrites=true&w=majority"
   )
-  .then(() => {app.listen(5000)})
+  .then(() => {app.listen(PORT)})
   .then(()=>{console.log("Database connected to port localhost 5000")})
   .catch((err)=>console.log(err))
 
-//CRuOAqn5vCzXm3pY
+if(process.env.NODE_ENV == 'production'){
+  app.use(express.static("frontend/build"))
+}
